7f7797bbd9237cbbbfc2c701a43bfc8410e77e2a
felixlutr <felixlutrovnik@gmail.com>
2024-09-12 23:38:00 +0300

    added some buttons, header, form with input for subscribe and started a blog item compo

---
+++ Components/BlogItem.jsx
@@ -0,0 +1,13 @@
+import Image from 'next/image';
+import React from 'react';
+import { blog_data } from '@/Assets/assets';
+
+const BlogItem = () => {
+  return (
+    <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]'>
+      <Image src={blog_data[0].image} alt='' width={400} height={400} className='border-b border-black'/>
+    </div>
+  );
+};
+
+export default BlogItem;
