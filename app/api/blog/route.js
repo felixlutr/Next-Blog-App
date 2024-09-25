99fbe494254b7528f77181dc84c22bc9c76412ff
felixlutr <felixlutrovnik@gmail.com>
2024-09-25 16:44:45 +0300

    finished working on admin panel tab, bloglist tab also finished successfully, starting work on subscriptions and subscriptions list in admin panel

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -2,49 +2,59 @@ import { ConnectDB } from "@/lib/config/db";
 import BlogModel from "@/lib/models/BlogModel";
 import { log } from "console";
 const { NextResponse } = require("next/server");
-import {writeFile} from 'fs/promises'
-
-
-
+import { writeFile } from "fs/promises";
+const fs = require('fs')
 
 const LoadDB = async () => {
-    await ConnectDB();
-}
+  await ConnectDB();
+};
 
 LoadDB();
 
-
 //API - to get all blogs
 export async function GET(request) {
-  
-  const blogs = await BlogModel.find({});
-
-
-  return NextResponse.json({blogs});
+  const blogId = request.nextUrl.searchParams.get("id");
+  if (blogId) {
+    const blog = await BlogModel.findById(blogId);
+    return NextResponse.json(blog);
+  } else {
+    const blogs = await BlogModel.find({});
+    return NextResponse.json({ blogs });
+  }
 }
 
-export async function POST(request){
-    const formData = await request.formData();
-    const timestamp = Date.now();
+export async function POST(request) {
+  const formData = await request.formData();
+  const timestamp = Date.now();
 
-    const image = formData.get('image');
-    const imageByteData = await image.arrayBuffer();
-    const buffer = Buffer.from(imageByteData);
-    const path = `./public/${timestamp}_${image.name}`
-    await writeFile(path,buffer);
-    const imgUrl = `/${timestamp}_${image.name}`
-    
-      const blogData = {
-        title:`${formData.get('title')}`,
-        description: `${formData.get('desctiption')}`,
-        category: `${formData.get('category')}`,
-        author: `${formData.get('author')}`,
-        image: `${imgUrl}`,
-        authorImg: `${formData.get('authorImg')}`
-      }
+  const image = formData.get("image");
+  const imageByteData = await image.arrayBuffer();
+  const buffer = Buffer.from(imageByteData);
+  const path = `./public/${timestamp}_${image.name}`;
+  await writeFile(path, buffer);
+  const imgUrl = `/${timestamp}_${image.name}`;
 
-      await BlogModel.create(blogData);
-      console.log('Blog Saved');
+  const blogData = {
+    title: `${formData.get("title")}`,
+    description: `${formData.get("desctiption")}`,
+    category: `${formData.get("category")}`,
+    author: `${formData.get("author")}`,
+    image: `${imgUrl}`,
+    authorImg: `${formData.get("authorImg")}`,
+  };
 
-    return NextResponse.json({success:true,msg:"Blog Added"})
+  await BlogModel.create(blogData);
+  console.log("Blog Saved");
+
+  return NextResponse.json({ success: true, msg: "Blog Added" });
+}
+
+//creating api to delete blog
+
+export async function DELETE(request){
+  const id = await request.nextUrl.searchParams.get('id');
+  const blog = await BlogModel.findById(id);
+  fs.unlink(`./public${blog.image}`,() => {});
+  await BlogModel.findByIdAndDelete(id);
+  return NextResponse.json({msg:"Blog Deleted!"})
 }
--- app/blogs/[id]/page.jsx
+++ app/blogs/[id]/page.jsx
@@ -5,18 +5,19 @@ import React, { useEffect, useState } from "react";
 import { assets } from "@/Assets/assets";
 import Footer from "@/Components/Footer";
 import Link from "next/link";
+import axios from "axios";
 
 const page = ({ params }) => {
   const [data, setData] = useState(null);
 
-  const fetchBlogData = () => {
-    for (let i = 0; i < blog_data.length; i++) {
-      if (Number(params.id) === blog_data[i].id) {
-        setData(blog_data[i]);
-        console.log(blog_data[i]);
-        break;
+  const fetchBlogData = async () => {
+    const response = await axios.get('/api/blog',{
+      params:{
+        id:params.id
       }
-    }
+    })
+    setData(response.data);
+
   };
 
   useEffect(() => {
@@ -46,7 +47,7 @@ const page = ({ params }) => {
           </h1>
           <Image
             className="mx-auto mt-6 border border-white rounded-full"
-            src={data.author_img}
+            src={data.authorImg}
             width={60}
             height={60}
             alt=""
