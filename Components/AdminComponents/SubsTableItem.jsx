93a4d8e2ab5e642562f98fbe44f564a5c8fdfdd1
felixlutr <felixlutrovnik@gmail.com>
2024-09-26 20:33:44 +0300

    homepage subscribe is working fine, now working on showing on admin subslist.

---
+++ Components/AdminComponents/SubsTableItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const SubsTableItem = () => {
+  return (
+    <tr className='bg-white border-b text-left'>
+      <th scope='row' className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap'>
+
+      </th>
+    </tr>
+  );
+};
+
+export default SubsTableItem;
